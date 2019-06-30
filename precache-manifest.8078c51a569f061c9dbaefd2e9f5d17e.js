d2d0882732308a7d2e8019a9b20e48f14b0eb53f
Henry Heberle <heberleh.public@gmail.com>
2019-06-30 19:38:42 -0300

    Updates

---
+++ precache-manifest.8078c51a569f061c9dbaefd2e9f5d17e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "4283419dfcb21426a12566746394caf9",
+    "url": "/comics-universe/static/media/sunMarvel.4283419d.svg"
+  },
+  {
+    "revision": "aab39a190a50d84eff258cc34968bd91",
+    "url": "/comics-universe/static/media/sunDefault.aab39a19.svg"
+  },
+  {
+    "revision": "3eda8d5a9cdd50ef8e78218d27e2f7e1",
+    "url": "/comics-universe/static/media/sunDc.3eda8d5a.svg"
+  },
+  {
+    "revision": "02c3897c20dfe5ccd2d45fcd9fa399ca",
+    "url": "/comics-universe/static/media/legend.02c3897c.png"
+  },
+  {
+    "revision": "b9bf7d104b8ccc02a554",
+    "url": "/comics-universe/static/js/runtime~main.4937bc1c.js"
+  },
+  {
+    "revision": "28f9a2dc37ab1887d772",
+    "url": "/comics-universe/static/js/main.96fedf73.chunk.js"
+  },
+  {
+    "revision": "18f240c2db2e491a73e3",
+    "url": "/comics-universe/static/js/2.9310b570.chunk.js"
+  },
+  {
+    "revision": "28f9a2dc37ab1887d772",
+    "url": "/comics-universe/static/css/main.b507c66d.chunk.css"
+  },
+  {
+    "revision": "18f240c2db2e491a73e3",
+    "url": "/comics-universe/static/css/2.90222e37.chunk.css"
+  },
+  {
+    "revision": "a911bf7ffeadeab372c779a6ba751c2a",
+    "url": "/comics-universe/index.html"
+  }
+];
